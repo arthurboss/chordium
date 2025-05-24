f290036d682d0853557e96420746d3ec732d894e
Arthur Boss <arthurboss@outlook.com>
2025-05-24 21:17:37 +0200

    fix: update Vite configuration to exclude backend for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -113,6 +113,14 @@ export default defineConfig(({ mode }) => {
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./src/test-setup.ts'],
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/backend/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'
+      ],
       css: true,
       pool: 'forks',
       poolOptions: {
