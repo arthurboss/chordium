bd00b21ae4105d6b59caf166fbc2b0efadfa22ce
Arthur Boss <arthurboss@outlook.com>
2025-08-06 10:59:22 +0200

    refactor: remove SearchTabProps type definition and related imports

--- frontend/src/components/tabs/search-tab.types.ts
+++
@@ -1,7 +0,0 @@
-import type { Song } from "@chordium/types";
-
-export interface SearchTabProps {
-  setMySongs?: React.Dispatch<React.SetStateAction<Song[]>>;
-  setActiveTab?: (tab: string) => void;
-  setSelectedSong?: React.Dispatch<React.SetStateAction<Song | null>>;
-}
